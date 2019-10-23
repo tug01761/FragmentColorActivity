ed608d2ab88426c2417e6cacf18b70ba27bbd555
Mike Lauw <tug01761@temple.edu>
2019-10-22 22:02:33 -0400

    sectionstatepageradapter done

--- app/src/main/java/temple/edu/fragmentcoloractivity/SectionsStatePagerAdapter.java
+++ app/src/main/java/temple/edu/fragmentcoloractivity/SectionsStatePagerAdapter.java
@@ -1,4 +1,38 @@
 package temple.edu.fragmentcoloractivity;
 
-public class SectionsStatePagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsStatePagerAdapter extends FragmentStatePagerAdapter
+{
+
+    private final List<Fragment> mFragmentList = new ArrayList<>(); //collection of the fragments
+    private final List<String> mFragmentTitleList = new ArrayList<>(); //collection of the fragment's title
+
+    public SectionsStatePagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    private void addFragment(Fragment fragment, String title)
+    {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
 }
